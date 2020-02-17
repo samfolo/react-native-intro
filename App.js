b37f68c4ea7f9ef52ef81e7908cc8b67e48413a4
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-02-17 13:39:49 +0000

    feat: add items to a list with <Text /> components and react hooks

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput 
           placeholder="Course Goal" 
-          style={styles.input} />
-        <Button title="Add" />
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal} />
+        <Button title="Add" onPress={addGoalHandler} />
       </View>
       <View>
-        
+        {courseGoals.map((goal, i) => <Text key={`${goal}${i}`}>{goal}</Text>)}
       </View>
     </View>
   );
